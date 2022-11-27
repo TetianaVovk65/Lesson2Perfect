9d64c1242d8375d9751acbc79eccb36c7cf18dda
TetianaVovk65 <johnbrook656@gmail.com>
2022-11-27 20:27:44 +0100

    new task

---
+++ Homework5.py
@@ -0,0 +1,19 @@
+#task1
+n = 5
+for i in range(0, n):
+    print('*' * (n - i) + ' ' * i)
+
+#task2
+n = 0
+for i in range(n, 6):
+    print('*' * (n + i) + ' ' * i)
+
+#task3
+n = 5
+for i in range(0, n):
+    print(' ' * i + '*' * (n - i) + ' ' * i)
+
+#task4
+n = 6
+for i in range(0, n):
+    print(' ' * (n-i) + '*' * i)
