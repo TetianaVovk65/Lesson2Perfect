f7e64eeedff4d78f0d16f59e4e54676402ac4e05
TetianaVovk65 <johnbrook656@gmail.com>
2022-12-26 16:32:32 +0100

    new task

---
+++ Homework12.py
@@ -0,0 +1,21 @@
+import math
+
+
+def Log2(x):
+    return (math.log10(x) /
+            math.log10(2))
+
+
+def isPowerOfTwo(n):
+    return math.ceil(Log2(n)) == math.floor(Log2(n))
+
+
+if isPowerOfTwo(33):
+    print("Yes")
+else:
+    print("No")
+
+if isPowerOfTwo(64):
+    print("Yes")
+else:
+    print("No")
